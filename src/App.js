94c63a376180e554bf00ca0fa6c15de298265dc7
tonnysanjidaakter <tonnysanjidaakter@gmail.com>
2022-05-01 15:04:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Device from './components/Device/Device';
+// import Tablet from './components/Tablet/Tablet';
+import Watch from './components/Watch/Watch';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Device name="uphone" price="13000"></Device>
+    <Watch></Watch>
+    {/* <Tablet></Tablet> */}
     </div>
   );
 }
